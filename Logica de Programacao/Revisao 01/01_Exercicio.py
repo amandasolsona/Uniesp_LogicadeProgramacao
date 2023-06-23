6452e5224578041abc24a4156fd76ffea015b951
Amanda Solsona <amandasolsona0@gmail.com>
2023-06-23 11:00:13 -0300

    Exercício referente a revisão da primeira aula

---
+++ Logica de Programacao/Revisao 01/01_Exercicio.py
@@ -0,0 +1,8 @@
+# Escreva um programa em Python que peça ao usuário
+# para digitar dois números e exiba a soma deles.
+
+num_01 = float(input('Por gentileza, digite o primeiro número para realização da soma: '))
+num_02 = float(input('Por gentileza, digite o segundo número para realização da soma: '))
+soma = num_01+num_02
+
+print(f'A soma dos dois números é {soma}.')
