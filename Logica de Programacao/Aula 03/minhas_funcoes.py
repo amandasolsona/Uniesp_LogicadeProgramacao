f7739eaa299031f311b0191bb8eee21f2bc00e48
Amanda Solsona <amandasolsona0@gmail.com>
2023-06-23 10:36:44 -0300

    Minhas funções

---
+++ Logica de Programacao/Aula 03/minhas_funcoes.py
@@ -0,0 +1,12 @@
+def somar(*args):
+    resultado = 0
+    for i in args:
+        resultado += i
+    return resultado
+
+
+def subtrair(*args):
+    resultado = 0
+    for i in args:
+        resultado -= i
+    return resultado
