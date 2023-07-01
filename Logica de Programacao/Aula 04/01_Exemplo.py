ceccc6bc762ca6b2e8697dff8ff6d6209e3086d5
AMANDA JESSIKA SOLSONA DE SOUZA WANDERLEY <amandasolsona@academicos.com>
2023-07-01 09:50:49 -0300

    Alteração

--- Logica de Programacao/Aula 04/01_Exemplo.py
+++ Logica de Programacao/Aula 04/01_Exemplo.py
@@ -1,4 +1,7 @@
+#Abrir e ler um arquivo
+
 with open('arquivo.TXT', 'r', encoding='utf-8') as file_object:
     conteudo = file_object.read()
     print(conteudo)
     print(f'Tipo: {type (conteudo)} \n{conteudo}')
+
